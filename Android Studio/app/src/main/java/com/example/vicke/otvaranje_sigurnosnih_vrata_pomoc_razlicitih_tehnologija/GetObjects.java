3b77693d89a3ec0760033e325256190885256e20
Karlo Vuljanko <vickedvuljo@gmail.com>
2018-11-20 15:03:47 +0100

    dinamic list created

---
+++ Android Studio/app/src/main/java/com/example/vicke/otvaranje_sigurnosnih_vrata_pomoc_razlicitih_tehnologija/GetObjects.java
@@ -0,0 +1,13 @@
+package com.example.vicke.otvaranje_sigurnosnih_vrata_pomoc_razlicitih_tehnologija;
+
+import java.util.List;
+
+public class GetObjects implements GetDataInterface{
+
+
+
+    @Override
+    public List<Object> getList() {
+        return null;
+    }
+}
