bca6ef076bc21ba549f3e581b9930ec4c285368a
evituri <enzo.vituri@gmail.com>
2018-12-04 11:14:00 +0100

    Added Access Service

--- restAPI/restAPI/Services/ObjectService.cs
+++ restAPI/restAPI/Services/ObjectService.cs
@@ -9,7 +9,7 @@ namespace restAPI.Services
 {
     public interface IObjectService
     {
-        AcObject getObject(string type, string objectName, int userId);
+        AcObject getObject(string type, string objectName);
     }
 
     public class ObjectService : IObjectService
@@ -21,9 +21,9 @@ namespace restAPI.Services
             _context = context;
         }
 
-        public AcObject getObject(string type, string objectName, int userId)
+        public AcObject getObject(string type, string objectName)
         {
-            AcObject acObject = (
+            AcObject returnObj = (
                 from obj in _context.AcObject
                 join ohs in _context.AcObjectHasTriggerType on obj.ObjId equals ohs.OhtObjId
                 join trgt in _context.AcTriggerType on ohs.OhtTrtId equals trgt.TrtId
@@ -31,35 +31,7 @@ namespace restAPI.Services
                 select obj
             ).SingleOrDefault();
 
-            if(acObject == null)
-            {
-                throw new AppException("Object not found.");
-            }
-            else if(acObject.ObjActivity == 0)
-            {
-                throw new AppException("Object not found.");
-            }
-            else if(acObject.ObjAuto == 0)
-            {
-                throw new AppException("Can't open object.");
-            }
-            else if (acObject.ObjOpen == 1)
-            {
-                throw new AppException("Object already open.");
-            }
-
-            AcAccess access = (
-                from acs in _context.AcAccess
-                where acs.AcsUsr.UsrId == userId && acs.AcsObj.ObjId == acObject.ObjId && acs.AcsValidFrom <= DateTime.Now && acs.AcsValidTo >= DateTime.Now
-                select acs
-            ).SingleOrDefault();
-
-            if(access == null)
-            {
-                throw new AppException("User has no access.");
-            }
-
-            return acObject;
+            return returnObj;
         }
     }
 }
